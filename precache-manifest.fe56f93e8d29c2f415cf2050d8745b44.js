b970a068621c29e4bce4ee8cf9980615cd13d55f
Thai Bach <thbach@gmail.com>
2021-01-20 18:28:56 -0800

    first

---
+++ precache-manifest.fe56f93e8d29c2f415cf2050d8745b44.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df0c62e9f8fea963dc3c8b787049ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07bccae351b16f0ca26d",
+    "url": "/static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "07bccae351b16f0ca26d",
+    "url": "/static/js/2.d5fa5789.chunk.js"
+  },
+  {
+    "revision": "b55fb870811f971aea60054c44fd53c6",
+    "url": "/static/js/2.d5fa5789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5daea95e07704175c9",
+    "url": "/static/js/main.567cc949.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6f6b718731898ee2342369431eb40411",
+    "url": "/static/media/challenge.6f6b7187.png"
+  },
+  {
+    "revision": "5a5daaf366d89cf648cf0fc2f60c098c",
+    "url": "/static/media/fireMed.5a5daaf3.png"
+  },
+  {
+    "revision": "601ecfffdfb6e3a031f80913f135c048",
+    "url": "/static/media/fireSmall.601ecfff.png"
+  },
+  {
+    "revision": "1ec3b69cfa2e6ca176e3ffd81e9543a3",
+    "url": "/static/media/plantMed.1ec3b69c.png"
+  },
+  {
+    "revision": "bdc991a8788b4de594b9bd558f4b0f8a",
+    "url": "/static/media/plantSmall.bdc991a8.png"
+  },
+  {
+    "revision": "ec223c949c776c3e66a18979e9045b8f",
+    "url": "/static/media/waterMed.ec223c94.png"
+  },
+  {
+    "revision": "e921b40ee5cb0799337d53336b19c596",
+    "url": "/static/media/waterSmall.e921b40e.png"
+  }
+]);
